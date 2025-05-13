d9bcf00e62a28ee8fe424bec940b1bdc32c8566d
eymeric <eymericdechelette@gmail.com>
2025-05-13 12:19:02 +0200

    feat: implement Game class for game logic and integrate with Model

---
+++ src/main/java/fr/polytech/pie/model/Game.java
@@ -0,0 +1,84 @@
+package fr.polytech.pie.model;
+
+import fr.polytech.pie.Consts;
+
+public class Game{
+    private Grid grid = new Grid(Consts.SIZE);
+    private CurrentPiece currentPiece;
+
+    public Game() {
+        this.currentPiece = PieceGenerator.generatePiece(grid.getSize());
+    }
+
+    public Grid getGrid() {
+        return grid;
+    }
+
+    public CurrentPiece getCurrentPiece() {
+        return currentPiece;
+    }
+
+    public void translateCurrentPiece(int dx, int dy) {
+        int originalX = currentPiece.getX();
+        int originalY = currentPiece.getY();
+
+        currentPiece.translate(dx, dy);
+
+        if (currentPiece.getX() < 0) {
+            currentPiece.setX(0);
+        }
+
+        if (currentPiece.getX() + currentPiece.getWidth() > grid.getSize()) {
+            currentPiece.setX(grid.getSize() - currentPiece.getWidth());
+        }
+
+        if (checkCollision()) {
+            currentPiece.setX(originalX);
+            currentPiece.setY(originalY);
+
+            if (dy > 0) {
+                grid.freezePiece(currentPiece);
+                currentPiece = PieceGenerator.generatePiece(grid.getSize());
+                System.out.println("New piece generated");
+
+                if (checkCollision()) {
+                    System.out.println("Fin du jeu !");
+                    resetGame();
+                }
+            }
+        }
+    }
+
+    public void resetGame() {
+        grid = new Grid(Consts.SIZE);
+        currentPiece = PieceGenerator.generatePiece(grid.getSize());
+    }
+
+    public boolean checkCollision() {
+        for (int i = 0; i < currentPiece.getHeight(); i++) {
+            for (int j = 0; j < currentPiece.getWidth(); j++) {
+                if (currentPiece.getPiece()[i][j]) {
+                    int x = currentPiece.getX() + j;
+                    int y = currentPiece.getY() + i;
+
+                    if (x < 0 || x >= grid.getSize() || y < 0 || y >= grid.getSize()) {
+                        return true;
+                    }
+
+                    if (grid.getValue(x, y)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public void rotateCurrentPiece() {
+        currentPiece.rotate((currentPiece) -> grid.checkCollision(currentPiece));
+
+    }
+
+
+
+}
--- src/main/java/fr/polytech/pie/model/Model.java
+++ src/main/java/fr/polytech/pie/model/Model.java
@@ -1,7 +1,5 @@
 package fr.polytech.pie.model;
 
-import fr.polytech.pie.Consts;
-
 import java.util.Observable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -9,124 +7,57 @@ import java.util.concurrent.TimeUnit;
 
 @SuppressWarnings("deprecation")
 public class Model extends Observable {
-    Grid grid = new Grid(Consts.SIZE);
-    CurrentPiece currentPiece;
-
+    private final Game game = new Game();
     public boolean[] keys = new boolean[5];
 
     public Model() {
-        currentPiece = PieceGenerator.generatePiece(grid.getSize());
-
         ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
-        scheduler.scheduleAtFixedRate(
-                () -> {
-                    if (keys[0]) {
-                        translateCurrentPiece(0, -1); // Down
-                    }
-                    if (keys[1]) {
-                        translateCurrentPiece(0, 1); // Up
-                    }
-                    if (keys[2]) {
-                        translateCurrentPiece(-1, 0); // Right
-                    }
-                    if (keys[3]) {
-                        translateCurrentPiece(1, 0); // Left
-                    }
-                    if (keys[4]) {
-                        rotateCurrentPiece(); // Rotate
-                    }
-                }, 0, 50, TimeUnit.MILLISECONDS
-        );
+        scheduler.scheduleAtFixedRate(() -> {
+            if (keys[0]) {
+                translateCurrentPiece(0, -1); // Down
+            }
+            if (keys[1]) {
+                translateCurrentPiece(0, 1); // Up
+            }
+            if (keys[2]) {
+                translateCurrentPiece(-1, 0); // Right
+            }
+            if (keys[3]) {
+                translateCurrentPiece(1, 0); // Left
+            }
+            if (keys[4]) {
+                rotateCurrentPiece(); // Rotate
+            }
+        }, 0, 50, TimeUnit.MILLISECONDS);
 
-        scheduler.scheduleAtFixedRate(
-                () -> translateCurrentPiece(0, 1), 0, 200, TimeUnit.MILLISECONDS
-        );
+        scheduler.scheduleAtFixedRate(() -> translateCurrentPiece(0, 1), 0, 200, TimeUnit.MILLISECONDS);
 
         Runtime.getRuntime().addShutdownHook(new Thread(scheduler::shutdown));
     }
 
     public void translateCurrentPiece(int dx, int dy) {
-        int originalX = currentPiece.getX();
-        int originalY = currentPiece.getY();
-
-        currentPiece.translate(dx, dy);
-
-        if (currentPiece.getX() < 0) {
-            currentPiece.setX(0);
-        }
-
-        if (currentPiece.getX() + currentPiece.getWidth() > grid.getSize()) {
-            currentPiece.setX(grid.getSize() - currentPiece.getWidth());
-        }
-
-        if (checkCollision()) {
-            currentPiece.setX(originalX);
-            currentPiece.setY(originalY);
-
-            if (dy > 0) {
-                grid.freezePiece(currentPiece);
-                currentPiece = PieceGenerator.generatePiece(grid.getSize());
-                System.out.println("New piece generated");
-
-                if (checkCollision()) {
-                    System.out.println("Fin du jeu !");
-                    resetGame();
-                }
-            }
-        }
-
+        game.translateCurrentPiece(dx, dy);
         setChanged();
         notifyObservers();
-    }
-
-    private void resetGame() {
-        grid = new Grid(Consts.SIZE);
-        currentPiece = PieceGenerator.generatePiece(grid.getSize());
-    }
-
-    private boolean checkCollision() {
-        for (int i = 0; i < currentPiece.getHeight(); i++) {
-            for (int j = 0; j < currentPiece.getWidth(); j++) {
-                if (currentPiece.getPiece()[i][j]) {
-                    int x = currentPiece.getX() + j;
-                    int y = currentPiece.getY() + i;
-
-                    if (x < 0 || x >= grid.getSize() || y < 0 || y >= grid.getSize()) {
-                        return true;
-                    }
-
-                    if (grid.getValue(x, y)) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
     }
 
     public void rotateCurrentPiece() {
-        currentPiece.rotate((currentPiece) -> grid.checkCollision(currentPiece));
-        setChanged();
-        notifyObservers();
-    }
-
-    public CurrentPiece getCurrentPiece() {
-        return currentPiece;
-    }
-
-    public void setCurrentPiece(CurrentPiece currentPiece) {
-        this.currentPiece = currentPiece;
+        game.rotateCurrentPiece();
         setChanged();
         notifyObservers();
     }
 
     public Grid getGrid() {
-        return grid;
+        return game.getGrid();
     }
 
     public void setKey(int key, boolean state) {
         if (key >= 0 && key < keys.length) {
             keys[key] = state;
         }
+    }
+
+    public CurrentPiece getCurrentPiece() {
+        return game.getCurrentPiece();
     }
 }
